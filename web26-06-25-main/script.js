60ad09b25e456a513c0c83460ac4c6b56a4b606c
Arthur Hassimyan <104658806+ArthurHassimyan@users.noreply.github.com>
2025-06-26 21:06:34 -0300

    Add files via upload

---
+++ web26-06-25-main/script.js
@@ -0,0 +1,19 @@
+let secreto = 5;
+let chute = 0; 
+let total = 0;
+let tentativa = 0;
+
+while(chute != secreto){
+    chute = parseInt(prompt("Escolha um número entre 1 e 10"))
+    total = total + chute;
+    tentativa++;
+
+    if(chute == secreto){
+        alert(`Acertou Media é ${total/tentativa}`)
+
+    }else if(chute > secreto){
+        alert(`O número secreto é menor que ${chute}. Tente novamente.`)s
+    }else{
+        alert(`O número secreto é maior que ${chute}. Tente novamente.`)
+    }
+}
